77053c64ca52458a42b1104f3c59caf1ab5c3496
Vincent Le <quyen.lechi@gmail.com>
2024-08-26 20:03:58 -0700

    feat(nvim): test

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- vim.keymap.set("n", "<leader>tn", "<cmd>TestNearest<cr>", { desc = "Test Nearest" })
+-- vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<cr>", { desc = "Test File" })
---
+++ nvim/.config/nvim/lua/plugins/test.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    opts = {
+      adapters = {
+        require("neotest-golang")({
+          go_test_args = {
+            "-v",
+            "-race",
+            "-count=1",
+            "-timeout=60s",
+            "-coverprofile=" .. vim.fn.getcwd() .. "/coverage.out",
+          },
+          dap_go_enabled = true,
+        }),
+        -- require("neotest-vim-test")({
+        --   ignore_file_types = { "go" },
+        -- }),
+      },
+    },
+  },
+  {
+    "vim-test/vim-test",
+    config = function()
+      vim.cmd([[
+      let test#strategy = 'neovim'
+    ]])
+
+      vim.g["test#go#gotest#options"] = "-v -race -count=1"
+    end,
+  },
+}
