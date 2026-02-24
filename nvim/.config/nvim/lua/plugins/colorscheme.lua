961dc31686b8caacded30af3527110fab566884f
Vincent Le <quyen.lechi@gmail.com>
2026-02-23 16:31:58 -0800

    chore(nvim): update colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,58 @@
 return {
-  --   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  --   {
-  --     "LazyVim/LazyVim",
-  --     opts = {
-  --       colorscheme = "catppuccin-latte",
-  --     },
-  --   },
-
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-      require("rose-pine").setup({})
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "latte", -- "latte", "frappe", "macchiato", "mocha"
+      auto_integrations = true,
+      custom_highlights = function(colors)
+        return {
+          ["@function"] = { italic = true },
+          ["@function.call"] = { italic = true },
+          -- Neovim >= 0.9 standard treesitter captures for methods
+          ["@function.method"] = { italic = true },
+          ["@function.method.call"] = { italic = true },
+          -- LSP semantic tokens for methods
+          ["@lsp.type.method"] = { italic = true },
+          ["@lsp.type.function"] = { italic = true },
+        }
+      end,
 
-      -- vim.cmd("colorscheme rose-pine")
-      -- vim.cmd("colorscheme rose-pine-main")
-      vim.cmd("colorscheme rose-pine-moon")
-      -- vim.cmd("colorscheme rose-pine-dawn")
-    end,
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = false,
+      dim_inactive = {
+        enabled = false,
+      },
+      styles = {
+        comments = { "italic" },
+        conditionals = { "bold" },
+        keywords = { "bold" },
+        functions = { "bold" },
+        types = { "bold" },
+      },
+    },
   },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-latte",
+    },
+  },
+
+  -- {
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
+  --   config = function()
+  --     require("rose-pine").setup({})
+
+  --     -- vim.cmd("colorscheme rose-pine")
+  --     -- vim.cmd("colorscheme rose-pine-main")
+  --     vim.cmd("colorscheme rose-pine-moon")
+  --     -- vim.cmd("colorscheme rose-pine-dawn")
+  --   end,
+  -- },
 }
--- nvim/.config/nvim/lua/plugins/neoscroll.lua
+++ nvim/.config/nvim/lua/plugins/neoscroll.lua
@@ -3,6 +3,17 @@ return {
   config = function()
     require("neoscroll").setup({
       cursor_scrolls_alone = false,
+      mappings = {
+        -- "<C-u>",
+        -- "<C-d>",
+        "<C-b>",
+        "<C-f>",
+        "<C-y>",
+        "<C-e>",
+        "zt",
+        "zz",
+        "zb",
+      },
     })
   end,
 }
